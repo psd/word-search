458adebcdf136c18402cf3de36b66f68957c41be
Paul Downey <psd@whatfettle.com>
2022-12-02 21:58:59 +0000

    Test loading multiple dictionaries

--- tests/test_load.py
+++ tests/test_load.py
@@ -3,7 +3,18 @@
 from wordsearch import WordSearcher
 
 
-def test_load():
+def test_load_package():
     searcher = WordSearcher()
+
     searcher.load_words()
     assert len(searcher.words) == 2315
+
+
+def test_load_path():
+    searcher = WordSearcher()
+
+    searcher.load_words("tests/ab.txt")
+    assert len(searcher.words) == 14
+
+    searcher.load_words("tests/et.txt")
+    assert len(searcher.words) == 18
